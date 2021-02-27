e509cf2110e6826276d9bb0e82dbfb5388fcb895
Casper Kristiansson <36133918+CasperKristiansson@users.noreply.github.com>
2021-02-27 14:12:16 +0100

    Added Simple Score, Paddle Hit, Reset Game

--- mipslabwork.c
+++ mipslabwork.c
@@ -10,9 +10,9 @@
 
    For copyright and licensing, see file COPYING */
 
-#include <stdint.h>  /* Declarations of uint_32 and the like */
-#include <pic32mx.h> /* Declarations of system-specific addresses etc */
-#include "mipslab.h" /* Declatations for these labs */
+#include <stdint.h>
+#include <pic32mx.h>
+#include "mipslab.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -24,31 +24,27 @@ float current_menu = 1;
 
 //Game specific variables
 float game_active = 0;
-float game_started = 0;
 float game_mode = 0;
+float score = 0;
 
 //Paddle specific variables
 float paddle_height = 8;
-float paddle_width = 4;
+float paddle_width = 3;
 float paddle_speed = 1;
 
 float paddle1_xPos = 0;
-float paddle1_yPos = 32 / 2 - 8;
+float paddle1_yPos = 32 / 2 - 2;
 
 float paddle2_xPos = 128 - 4;
-float paddle2_yPos = 32 / 2 - 8;
-
+float paddle2_yPos = 32 / 2 - 2;
 
 //Ball specific variables
 float ball_size = 4;
-float ball_speedx = 1;
+float ball_speedx = 0;
 float ball_speedy = 0;
 
-float ball_xPos = 128 / 2 - 4;
+float ball_xPos = 128 / 2 - 5;
 float ball_yPos = 32 / 2;
-
-
-
 
 /* Interrupt Service Routine */
 void user_isr( void )
@@ -67,15 +63,12 @@ void labinit( void )
 }
 
 // Set array slots to ones or zeros in the 2D array.
-void setPixelArray(int xPos, int yPos, int width, int height)
-{
+void setPixelArray(int xPos, int yPos, int width, int height) {
   int row, column;
-  for (row = 0; row < 32; row++)
-  { // y-axis
-    for (column = 0; column < 128; column++)
-    { // x-axis
-      if (row >= yPos && row <= (yPos + height) && column >= xPos && column <= (xPos + width))
-      {
+
+  for (row = 0; row < 32; row++) {
+    for (column = 0; column < 128; column++) {
+      if (row >= yPos && row <= (yPos + height) && column >= xPos && column <= (xPos + width)) {
         display[row][column] = 1;
       }
     }
@@ -83,63 +76,39 @@ void setPixelArray(int xPos, int yPos, int width, int height)
 }
 
 // Translate the 2D array into the 1D array with 512 slots.
-void translateToImage()
-{
+void translateToImage() {
   int page, column, row, c, k;
-  uint8_t powerOfTwo = 1; // Interval: 2^0, 2^1 ... to ... 2^7.
+  uint8_t powerOfTwo = 1;
   uint8_t oledNumber = 0;
   int survivalMode = 0;
-  for (page = 0; page < 4; page++)
-  {
-    for (column = 0; column < 128; column++)
-    {
+
+  for(page = 0; page < 4; page++) {
+    for(column = 0; column < 128; column++) {
       powerOfTwo = 1;
       oledNumber = 0;
-      for (row = 0; row < 8; row++)
-      {
-        if (display[8 * page + row][column])
-        {
+
+      for(row = 0; row < 8; row++) {
+        if(display[8 * page + row][column]) {
           oledNumber |= powerOfTwo;
         }
         powerOfTwo <<= 1;
       }
-      // Display score in survival mode (Write 8-width letters, one for every time the column is a multiple of 8)
-      
-      if (survivalMode && page == 0)
-      {
-        if (column % 8 == 0)
-        {
-          c = textbuffer[page][column / 8];
-        }
-        if (!(c & 0x80))
-        {
-          oledNumber |= font[c * 8 + column % 8]; // Set the hexadecimal number from font[] array and assign to the oledNumber variable.
-        }
-      }
-
       oled_display[column + page * 128] = oledNumber;
     }
   }
 }
 
-
-void clearDisplay()
-{
-  /*CLEARS ALL elements in arrays and sets 0 on them.
-    memset(display, 0, sizeof(display));
-    memset(oled_display, 0, sizeof(oled_display));
-  */
+void clearDisplay() {
   int row, column, i;
-  for (row = 0; row < 32; row++)
-  {
-    for (column = 0; column < 128; column++)
-    {
-      display[row][column] = 0; // Clear the array
+
+  for(row = 0; row < 32; row++) {
+    for(column = 0; column < 128; column++) {
+      display[row][column] = 0;
     }
   }
-  for (i = 0; i < 512; i++)
-  {
-    oled_display[i] = 0; // Clear the array
+
+  for (i = 0; i < 512; i++) {
+    oled_display[i] = 0;
   }
 }
 
@@ -190,7 +159,7 @@ void menu(btns) {
   }
 
   display_update();
-  delay(200);
+  delay(100);
 }
 
 void player_movement(btns) {
@@ -209,6 +178,65 @@ void player_movement(btns) {
   }
 }
 
+void paddle_hit() {
+  if (ball_xPos == paddle_width) {
+    if (((ball_yPos + ball_size) > paddle1_yPos) && (ball_yPos) < (paddle1_yPos + paddle_height)) {
+      ball_speedx = -(ball_speedx);
+    }
+    else {
+      goal(2);
+    }
+  }
+
+  if (ball_xPos == (128 - paddle_width - 4)) {
+    if (((ball_yPos + ball_size) > paddle2_yPos) && (ball_yPos) < (paddle2_yPos + paddle_height)) {
+      ball_speedx = -(ball_speedx);
+    }
+    else {
+      goal(1);
+    }
+  }
+}
+
+void goal(player) {
+  if (player == 1) {
+    score += 1;
+  }
+  if (player == 2) {
+    score -= 1;
+  }
+  reset_game();
+}
+
+void reset_game() {
+  paddle1_xPos = 0;
+  paddle1_yPos = 32 / 2 - 2;
+
+  paddle2_xPos = 128 - 4;
+  paddle2_yPos = 32 / 2 - 2;
+
+  ball_xPos = 128 / 2 - 5;
+  ball_yPos = 32 / 2;
+
+  if (score > 0) {
+    ball_speedx = -1;
+  }
+  else if (score < 0) {
+    ball_speedx = 1;
+  }
+  else {
+    ball_speedx = 1;
+  }
+
+  clearDisplay();
+  setPixelArray(paddle1_xPos, paddle1_yPos, paddle_width, paddle_height);
+  setPixelArray(paddle2_xPos, paddle2_yPos, paddle_width, paddle_height);
+  setPixelArray(ball_xPos, ball_yPos, ball_size, ball_size);
+  translateToImage();
+  display_image(0, oled_display);
+  delay(1000);
+}
+
 void string_clear() {
   display_string(0, "");
   display_string(1, "");
@@ -219,6 +247,10 @@ void string_clear() {
 }
 
 void ball_movement() {
+  if (!ball_speedx) {
+    ball_speedx = 1;
+    delay(1000);
+  }
   ball_xPos += ball_speedx;
   ball_yPos += ball_speedy;
 }
@@ -227,6 +259,7 @@ void two_player(btns) {
 
   player_movement(btns);
   ball_movement();
+  paddle_hit();
 
   clearDisplay();
   setPixelArray(paddle1_xPos, paddle1_yPos, paddle_width, paddle_height);
@@ -237,7 +270,7 @@ void two_player(btns) {
 }
 
 void one_player(btns) {
-  
+
 }
 
 /* This function is called repetitively from the main program */
